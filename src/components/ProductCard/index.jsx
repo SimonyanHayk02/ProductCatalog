1a2dfa6cb3742cecad3babaad8d58c8429a73395
Hayk Simonyan <hayk.simonyan.2002@gmail.com>
2025-09-15 23:29:11 +0400

    fix: icon fix

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -72,7 +72,7 @@ const ProductCard = memo(({ product }) => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth={2}
-                d="M3 3h2l.4 2M7 13h10l4-8H5.4m0 0L7 13m0 0l-2.5 5M7 13l2.5 5m6-5v6a2 2 0 01-2 2H9a2 2 0 01-2-2v-6m8 0V9a2 2 0 00-2-2H9a2 2 0 00-2 2v4.01"
+                d="M3 3h2l.4 2M7 13h10l4-8H5.4m0 0L7 13m0 0l-1.8 3.6a.8.8 0 00.8 1.2h9m-6 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
               />
             </svg>
             <span>Add</span>
